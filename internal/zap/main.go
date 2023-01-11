e56d2e3fe936c9b5cc82179eea6b9ddb91a6e58d
takokun <takokun.778@gmail.com>
2023-01-12 00:09:40 +0900

    zap

--- internal/zap/main.go
+++ internal/zap/main.go
@@ -4,7 +4,11 @@ import "go.uber.org/zap"
 
 func main() {
 	plog, _ := zap.NewProduction()
-	defer plog.Sync()
+	defer func() {
+		if err := plog.Sync(); err != nil {
+			panic(err)
+		}
+	}()
 
 	plog.Debug("debug", zap.String("id", "1"))
 	plog.Info("info", zap.String("id", "1"))
@@ -12,7 +16,11 @@ func main() {
 	plog.Error("error", zap.String("id", "1"))
 
 	dlog, _ := zap.NewDevelopment()
-	defer dlog.Sync()
+	defer func() {
+		if err := dlog.Sync(); err != nil {
+			panic(err)
+		}
+	}()
 
 	dlog.Debug("debug", zap.String("id", "1"))
 	dlog.Info("info", zap.String("id", "1"))
